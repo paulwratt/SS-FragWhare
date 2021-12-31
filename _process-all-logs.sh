909f5a394642a570f059201be9ede7091e7bedf9
Paul Wratt <paul.wratt@gmail.com>
2022-01-01 02:58:10 +1300

    _process-all-logs.sh: initial import and REPLACE substitutions

---
+++ _process-all-logs.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+cd "SSFWROOT/PREFIX_tools-dir" ;
+
+./FILEPRE_process-logs.sh;
+./FILEPRE_process-haxs.sh;
+./FILEPRE_process-errors.sh;
+./FILEPRE_process-browsers.sh;
+
+./FILEPRE_update-hourly-sshd.sh;
+
