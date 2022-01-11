603cc4e8983357165e4ce8ae0c9768cf5958cd0f
Paul Wratt <paul.wratt@gmail.com>
2022-01-11 16:02:38 +1300

    _remove-blocked-ipv4.sh: added range removal
    This is ontop of the fact that _block-ipv4-range.sh already
    has a "del" option.

--- _remove-blocked-ipv4.sh
+++ _remove-blocked-ipv4.sh
@@ -2,9 +2,37 @@
 
 ### Remove IPv4 via `ip`
 
-B=$(basename $1);
+B=$(basename $1 2>/dev/null);
 if [ "$B" = "" ]; then
-  echo "$0 127.0.0.254";
+  echo "delete block IPv4 address or range"
+  echo "usage: $0 127.0.0.254";
+  exit;
+fi;
+
+if [ ! "$B" = "$1" -a "$(echo "$1" | grep / 2>/dev/null)" = "$1" ]; then
+  FL="TRUE";
+  L=$(grep -nm1 "^$1" ../b/ipv4/.ranges.ipv4 | cut -d \: -f 1);
+  if [ "$L" = "" ]; then
+    echo "warning: range not in blocklist";
+    FL="FALSE";
+  else
+    sed -i "${L}d" ../b/ipv4/.ranges.ipv4 ;
+    echo "range: deleted from blocklist";
+  fi;
+  FB="TRUE";
+  B="$(ip r | grep / | grep -m1 "blackhole $1")";
+  if [ "$B" = "" ]; then
+    echo "warning: range not already blocked";
+    FB="FALSE";
+  else
+    ip route del $1;
+    echo "range: deleted from blackhole list";
+  fi;
+  if [ "$FL" = "FALSE" -a "$FB" = "FALSE" ]; then
+    echo "error: range not found: $1";
+  else
+    echo "range: removed: $1";
+  fi;
   exit;
 fi;
 
