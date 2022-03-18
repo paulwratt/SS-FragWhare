85df5a65645ff75ce91b736d6f10bf48cfa5bb51
Paul Wratt <paul.wratt@gmail.com>
2022-03-18 14:52:30 +1300

    _block-nginx-access.sh: update with more "common" attempts 22-03-18

--- _block-nginx-access.sh
+++ _block-nginx-access.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
+# NOTE: if the file IS found, it _wont_ appear in 'haxor-access.log',
+#       but it _will_ appear in 'nginx-access.log'.
+
 cd "SSFWROOT/PREFIX_tools-dir" ;
+
 # caught "litteral"  ;
 grep "x0" "SSFWROOT/PREFIX_log-dir/nginx-access.log" | cut -d \  -f 1 | xargs -n 1 -I {} ./PREFIX_block-ipv4-addr.sh {} false;
+grep "passwd" "SSFWROOT/PREFIX_log-dir/nginx-access.log" | cut -d \  -f 1 | xargs -n 1 -I {} ./PREFIX_block-ipv4-addr.sh {} false;
 
 # "not supported"  ;
 #grep '\" 400 \|\" 405 ' "SSFWROOT/PREFIX_log-dir/nginx-access.log" | cut -d \  -f 1 | xargs -n 1 -I {} ./PREFIX_block-ipv4-addr.sh {} false;
@@ -10,9 +15,30 @@ grep "x0" "SSFWROOT/PREFIX_log-dir/nginx-access.log" | cut -d \  -f 1 | xargs -n
 # caught "not supported"  ;
 grep '\" 400 \|\" 405 \|\" 200 0 ' "SSFWROOT/PREFIX_log-dir/haxors-access.log" | cut -d \  -f 1 | xargs -n 1 -I {} ./PREFIX_block-ipv4-addr.sh {} false;
 
-# caught "Netgear" and "Mozi"  ;
+
+### Exploits ###
+
+# Solar Winds
+grep "solr" "SSFWROOT/PREFIX_log-dir/haxors-access.log" | cut -d \  -f 1 | xargs -n 1 -I {} ./PREFIX_block-ipv4-addr.sh {} false;
+
+# Windows RPC
+grep "xlmrpc" "SSFWROOT/PREFIX_log-dir/haxors-access.log" | cut -d \  -f 1 | xargs -n 1 -I {} ./PREFIX_block-ipv4-addr.sh {} false;
+
+
+### Hardware Specific ###
+
+# generic direct access
+grep "x0" "SSFWROOT/PREFIX_log-dir/haxors-access.log" | cut -d \  -f 1 | xargs -n 1 -I {} ./PREFIX_block-ipv4-addr.sh {} false;
+grep "passwd" "SSFWROOT/PREFIX_log-dir/haxors-access.log" | cut -d \  -f 1 | xargs -n 1 -I {} ./PREFIX_block-ipv4-addr.sh {} false;
+grep "wget" "SSFWROOT/PREFIX_log-dir/haxors-access.log" | cut -d \  -f 1 | xargs -n 1 -I {} ./PREFIX_block-ipv4-addr.sh {} false;
+grep "admin" "SSFWROOT/PREFIX_log-dir/haxors-access.log" | cut -d \  -f 1 | xargs -n 1 -I {} ./PREFIX_block-ipv4-addr.sh {} false;
+grep "setup" "SSFWROOT/PREFIX_log-dir/haxors-access.log" | cut -d \  -f 1 | xargs -n 1 -I {} ./PREFIX_block-ipv4-addr.sh {} false;
+grep "config" "SSFWROOT/PREFIX_log-dir/haxors-access.log" | cut -d \  -f 1 | xargs -n 1 -I {} ./PREFIX_block-ipv4-addr.sh {} false;
+grep "getuser" "SSFWROOT/PREFIX_log-dir/haxors-access.log" | cut -d \  -f 1 | xargs -n 1 -I {} ./PREFIX_block-ipv4-addr.sh {} false;
+
+# caught "Netgear" and "Mozi" and "jaws"
 # https://blog.lumen.com/new-mozi-malware-family-quietly-amasses-iot-bots/
-grep 'setup.cgi?\|/shell?' "SSFWROOT/PREFIX_log-dir/haxors-access.log" | cut -d \  -f 1 | xargs -n 1 -I {} ./PREFIX_block-ipv4-addr.sh {} false;
+grep 'setup.cgi?\|shell?' "SSFWROOT/PREFIX_log-dir/haxors-access.log" | cut -d \  -f 1 | xargs -n 1 -I {} ./PREFIX_block-ipv4-addr.sh {} false;
 
 # BOA webserver exploit on fibre routers
 # https://www.theregister.com/2020/04/16/fiber_routers_under_fire/
@@ -26,22 +52,33 @@ grep '/HNAP1' /work/www/l/haxors-access.log | cut -d \  -f 1 | xargs -n 1 -I {} 
 grep '/GponForm/diag_Form' "SSFWROOT/PREFIX_log-dir/haxors-access.log" | cut -d \  -f 1 | xargs -n 1 -I {} ./PREFIX_block-ipv4-addr.sh {} false;
 
 
-### remark these if needed (eg #grep ..)
+### remark these if not needed (eg #grep ..)
 
 # default "WordPress"
-grep '/wp-login.php' "SSFWROOT/PREFIX_log-dir/haxors-access.log" | cut -d \  -f 1 | xargs -n 1 -I {} ./PREFIX_block-ipv4-addr.sh {} false;
-grep '/wp-content/' "SSFWROOT/PREFIX_log-dir/haxors-access.log" | cut -d \  -f 1 | xargs -n 1 -I {} ./m68k-svr_block-ipv4-addr.sh {} false;
+grep '/wp-' "SSFWROOT/PREFIX_log-dir/haxors-access.log" | cut -d \  -f 1 | xargs -n 1 -I {} ./PREFIX_block-ipv4-addr.sh {} false;
+#grep '/wp-login.php' "SSFWROOT/PREFIX_log-dir/haxors-access.log" | cut -d \  -f 1 | xargs -n 1 -I {} ./PREFIX_block-ipv4-addr.sh {} false;
+#grep '/wp-content/' "SSFWROOT/PREFIX_log-dir/haxors-access.log" | cut -d \  -f 1 | xargs -n 1 -I {} ./PREFIX_block-ipv4-addr.sh {} false;
+#grep '/wp-includes/' "SSFWROOT/PREFIX_log-dir/haxors-access.log" | cut -d \  -f 1 | xargs -n 1 -I {} ./PREFIX_block-ipv4-addr.sh {} false;
 
 # default "phpMyAdmin"
-grep '/phpMyAdmin\|/phpmyadmin' "SSFWROOT/PREFIX_log-dir/haxors-access.log" | cut -d \  -f 1 | xargs -n 1 -I {} ./PREFIX_block-ipv4-addr.sh {} false;
+grep 'phpMyAdmin\|MyAdmin\|phpmyadmin\|pma\|PMA' "SSFWROOT/PREFIX_log-dir/haxors-access.log" | cut -d \  -f 1 | xargs -n 1 -I {} ./PREFIX_block-ipv4-addr.sh {} false;
 
-# might be useful (?)
-grep '/.env' "SSFWROOT/PREFIX_log-dir/haxors-access.log" | cut -d \  -f 1 | xargs -n 1 -I {} ./PREFIX_block-ipv4-addr.sh {} false;
+# Others - might be useful (?)
+grep '\.env' "SSFWROOT/PREFIX_log-dir/haxors-access.log" | cut -d \  -f 1 | xargs -n 1 -I {} ./PREFIX_block-ipv4-addr.sh {} false;
+grep '/\.aws' "SSFWROOT/PREFIX_log-dir/haxors-access.log" | cut -d \  -f 1 | xargs -n 1 -I {} ./PREFIX_block-ipv4-addr.sh {} false;
+grep '/\.DS_Store' "SSFWROOT/PREFIX_log-dir/haxors-access.log" | cut -d \  -f 1 | xargs -n 1 -I {} ./PREFIX_block-ipv4-addr.sh {} false;
+grep '/\.ftpconfig' "SSFWROOT/PREFIX_log-dir/haxors-access.log" | cut -d \  -f 1 | xargs -n 1 -I {} ./PREFIX_block-ipv4-addr.sh {} false;
 
-# not sure about this 
+
+# not sure about this (NOTE: _these_ are common now - 22/03/18) 
 # mostly this is one user with '*.linodeusercontent.com' spinups
 # using 'python-requests/2.9.1' on unique location Linode servers
-# since August 2021 (see: git-contfig.log.txt)
-#grep '\.git/config' "SSFWROOT/PREFIX_log-dir/haxors-access.log" | cut -d \  -f 1 | xargs -n 1 -I {} ./PREFIX_block-ipv4-addr.sh {} false;
+# since August 2021 (see: git-config.log.txt)
+grep '\.git/config' "SSFWROOT/PREFIX_log-dir/haxors-access.log" | cut -d \  -f 1 | xargs -n 1 -I {} ./PREFIX_block-ipv4-addr.sh {} false;
+grep '\.git/HEAD' "SSFWROOT/PREFIX_log-dir/haxors-access.log" | cut -d \  -f 1 | xargs -n 1 -I {} ./PREFIX_block-ipv4-addr.sh {} false;
+grep '\.svn' "SSFWROOT/PREFIX_log-dir/haxors-access.log" | cut -d \  -f 1 | xargs -n 1 -I {} ./PREFIX_block-ipv4-addr.sh {} false;
+grep '\.hg' "SSFWROOT/PREFIX_log-dir/haxors-access.log" | cut -d \  -f 1 | xargs -n 1 -I {} ./PREFIX_block-ipv4-addr.sh {} false;
+grep '\.travis' "SSFWROOT/PREFIX_log-dir/haxors-access.log" | cut -d \  -f 1 | xargs -n 1 -I {} ./PREFIX_block-ipv4-addr.sh {} false;
+grep '\.vscode' "SSFWROOT/PREFIX_log-dir/haxors-access.log" | cut -d \  -f 1 | xargs -n 1 -I {} ./PREFIX_block-ipv4-addr.sh {} false;
 
 
